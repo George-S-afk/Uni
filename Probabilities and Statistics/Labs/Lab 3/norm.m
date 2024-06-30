5c619189d0c611f8a68308b1a5dfa8b1d5284e6d
George-S-afk <barageorge80@gmail.com>
2024-07-01 01:44:34 +0300

    Delete Probabilities and Statistics directory

--- Probabilities and Statistics/Labs/Lab 3/norm.m
+++
@@ -1,7 +0,0 @@
-p = input("p=");
-for i = 1 : 10
-  n = i*5;
-  v = 0 : n;
-  plot(v, binopdf(v, n, p)
-  pause(1);
-endfor
--- Probabilities and Statistics/Labs/Lab 3/poiss.m
+++
@@ -1,4 +0,0 @@
-p = input("p=");
-n = input("n=");
-v = 0 : n;
-plot(v, binopdf(v, n, p), 'm', v, poisspdf(v, n*p), 'b')
